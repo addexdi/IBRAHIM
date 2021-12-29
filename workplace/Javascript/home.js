94b34566d8237fa9d00ecd24c0438f8f1fa85681
Adam Ibrahim <onelordad@gmail.com>
2021-12-29 10:41:57 +0100

    Number

--- workplace/Javascript/home.js
+++ workplace/Javascript/home.js
@@ -1,3 +1,15 @@
 console.log("Hello");
-        console.log("hello");
-        alert("yooo")
+       // console.log("hello");
+       // alert("yooo")
+       // This is inline comment
+
+       // variable
+       var b = "smoothie";
+       console.log(b);
+
+       var someNumber = 45;
+       console.log(someNumber);
+
+       var age = prompt("what is your age?")
+
+       document.getElementById('someText').innerHTML = age;
