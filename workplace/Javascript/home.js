5e9ca923e3eec7ed8ec5a6ffb721ebe4a8014dd9
Adam Ibrahim <onelordad@gmail.com>
2021-12-29 10:59:19 +0100

    function

--- workplace/Javascript/home.js
+++ workplace/Javascript/home.js
@@ -4,12 +4,30 @@ console.log("Hello");
        // This is inline comment
 
        // variable
-       var b = "smoothie";
-       console.log(b);
+       // var b = "smoothie";
+       // console.log(b);
 
-       var someNumber = 45;
-       console.log(someNumber);
+      // var someNumber = 45;
+      // console.log(someNumber);
 
-       var age = prompt("what is your age?")
+      // var age = prompt("what is your age?")
 
-       document.getElementById('someText').innerHTML = age;
+     //  document.getElementById('someText').innerHTML = age;
+
+       // Number in Javascript.
+       var num1 = 10;
+
+       // Increment num by 1
+       num1++;
+
+       // Decrement num1 by 1
+       num1--;
+       console.log(num1);
+
+       // Divide /, multiply *, remainder %
+       console.log(num1 %  6);
+
+       // Increment/Decrement by any number.
+       num1 += 10;
+       console.log(num1)
+       
