22d8063cf31a53c0d687d7f47159888e2f0957dd
Adam Ibrahim <onelordad@gmail.com>
2021-12-29 11:28:53 +0100

    While and For

--- workplace/Javascript/home.js
+++ workplace/Javascript/home.js
@@ -29,5 +29,46 @@ console.log("Hello");
 
        // Increment/Decrement by any number.
        num1 += 10;
-       console.log(num1)
+       console.log(num1);
+
+       /* Functions
+       1. create a function
+       2. call a function
+       */
+
+       //create
+       function fun() {
+           console.log('this is a function');
+       }
+
+       // Call
+       fun();
+
+       /* Let's create a function that take in a name and says hello followed by your name
+
+       for example
+
+       Name: "Adam"
+       Return: "Hello Adam"
+       */
+
+       function greeting(yourName) {
+           var result = 'Hello' + ' ' + yourName;  // String Concatenation
+           console.log(result);
+       }
        
+       var name = prompt('What is your name');
+       greeting(name);
+
+       // How do arguments work in functions?
+       // How do we add two numbers together in a function?
+
+       function sumNumbers(num1, num2) {
+           var result = num1 + num2;
+           console.log(result);
+       }
+
+       sumNumbers(10, 10);
+
+
+
